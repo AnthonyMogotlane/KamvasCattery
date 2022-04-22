40acc4b593809cc5fbcddaeea377bc8ca7c6f688
Anthony <almogotlane@gmail.com>
2022-04-22 15:11:57 +0200

    modify weekly_wages/weekly_wages.js

--- weekly_wages/weekly_wages.js
+++ weekly_wages/weekly_wages.js
@@ -1,3 +1,10 @@
 function weeklyWage(band, weeklyHours) {
+    let sum = weeklyHours.split(",").map(dayHours => {
+        if(band === "A") return parseInt(dayHours) * 23;
+        if(band === "B") return parseInt(dayHours) * 37;
+        if(band === "C") return parseInt(dayHours) * 51;
+        return 0;
+    }).reduce((total, num) => total += num);
 
+    return (weeklyHours.split(",").length < 7) ? "missing day" : sum;
 };
