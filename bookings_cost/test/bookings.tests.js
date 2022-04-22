526b421f20ab0c8d29a662a7c7a01ddfdabeff51
Anthony <almogotlane@gmail.com>
2022-04-22 12:54:18 +0200

    update bookings_cost

--- bookings_cost/test/bookings.tests.js
+++ bookings_cost/test/bookings.tests.js
@@ -1,1 +1,13 @@
+describe("The bookingCost function", () => {
+    it("should return the total price of the days booked", () => {
+        assert.deepEqual(bookingCost({check_in_day: 'Monday', duration: 3, cat_name : 'Snowy'}), 135);
+    })
 
+    it("should return the total price with a discout of 15% for the bookings on Wednesday", () => {
+        assert.deepEqual(bookingCost({check_in_day: 'Wednesday', duration: 3, cat_name : 'Snowy'}), 101.25);
+    })
+
+    it("should return the total price with a discout of 25% for the bookings on Friday", () => {
+        assert.deepEqual(bookingCost({check_in_day: 'Wednesday', duration: 3, cat_name : 'Snowy'}), 101.25);
+    })
+})
