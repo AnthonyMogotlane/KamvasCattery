4c7a92fd4fccadc05382af81c8971991261c0bab
Anthony <almogotlane@gmail.com>
2022-04-22 15:27:55 +0200

    bookings_cost/test/bookings.tests.js

--- bookings_cost/test/bookings.tests.js
+++ bookings_cost/test/bookings.tests.js
@@ -1,13 +1,13 @@
 describe("The bookingCost function", () => {
-    it("should return the total price of the days booked", () => {
-        assert.deepEqual(bookingCost({check_in_day: 'Monday', duration: 3, cat_name : 'Snowy'}), 135);
+    it("should return the total price of the days booked for bookings made on Monday", () => {
+        assert.deepEqual(bookingCost({ check_in_day: 'Monday', duration: 3, cat_name: 'Snowy' }), 135);
     })
 
-    it("should return the total price with a discout of 15% for the bookings on Wednesday", () => {
-        assert.deepEqual(bookingCost({check_in_day: 'Wednesday', duration: 3, cat_name : 'Snowy'}), 101.25);
+    it("should return the total price with a discout of 15% for the bookings made on Wednesday", () => {
+        assert.deepEqual(bookingCost({ check_in_day: 'Wednesday', duration: 3, cat_name: 'Snowy' }), 101.25);
     })
 
-    it("should return the total price with a discout of 25% for the bookings on Friday", () => {
-        assert.deepEqual(bookingCost({check_in_day: 'Wednesday', duration: 3, cat_name : 'Snowy'}), 101.25);
+    it("should return the total price with a discout of 25% for the bookings made on Friday", () => {
+        assert.deepEqual(bookingCost({ check_in_day: 'Wednesday', duration: 3, cat_name: 'Snowy' }), 101.25);
     })
 })
